f61705cda4ff80095a881fab7935388c8df0877a
Minkyu Cho <mrnoname@naver.com>
2017-01-23 00:29:21 +0900

    Replace V8ObjectUtil to builtin stuff

--- src/main/scala/com/github/pitzcarraldo/spring/view/node/NodeViewRenderer.scala
+++ src/main/scala/com/github/pitzcarraldo/spring/view/node/NodeViewRenderer.scala
@@ -27,23 +27,24 @@ import java.io.File
 import java.util
 import java.util.concurrent.TimeUnit
 
-import com.eclipsesource.v8._
+import com.eclipsesource.v8.utils.V8ObjectUtils
+import com.eclipsesource.v8.{V8Value, _}
 import com.google.common.cache.{CacheBuilder, CacheLoader}
+import com.google.common.collect.ImmutableMap
 
 /**
   * @author Minkyu Cho (pitzcarraldo@gmail.com)
   */
 class NodeViewRenderer {
   def render(template: NodeViewTemplate): util.Map[String, AnyRef] = {
-    val nodeJS = NodeJS.createNodeJS(null)
+    val nodeJS = NodeJS.createNodeJS
+    val runtime = nodeJS.getRuntime
     val render: V8Function = nodeJS.require(new File(template.path)).asInstanceOf[V8Function]
-    val runtime = render.getRuntime
-    val objectUtils = new V8ObjectUtils(runtime)
-    val v8Model = objectUtils.fromMap(template.model)
+    val v8Model = V8ObjectUtils.toV8Object(runtime, template.model)
     var asyncResponse: util.Map[String, AnyRef] = null
     val callback = new V8Function(render.getRuntime, new JavaCallback() {
       def invoke(receiver: V8Object, parameters: V8Array): AnyRef = {
-        asyncResponse = objectUtils.toResponse(parameters.get(0))
+        asyncResponse = toResponse(runtime, parameters.get(0))
         receiver.release()
         parameters.release()
         null
@@ -52,7 +53,7 @@ class NodeViewRenderer {
     val args = new V8Array(runtime)
     args.push(v8Model)
     args.push(callback)
-    val syncResponse = objectUtils.toResponse(render.call(null, args))
+    val syncResponse = toResponse(runtime, render.call(null, args))
     while (nodeJS.isRunning) {
       nodeJS.handleMessage
     }
@@ -62,6 +63,48 @@ class NodeViewRenderer {
     render.release()
     nodeJS.release()
     if (asyncResponse != null) asyncResponse else syncResponse
+  }
+
+  private def toResponse(runtime: V8, value: AnyRef): util.Map[String, AnyRef] = value match {
+    case value: String => ImmutableMap.of("body", value)
+    case value: V8Object =>
+      val builder = new ImmutableMap.Builder[String, AnyRef]()
+      if (!value.isUndefined) {
+        if (value.contains("headers")) {
+          val headers = value.getObject("headers")
+          println(V8ObjectUtils.getValue(value, "headers"))
+          builder.put("headers", V8ObjectUtils.toMap(headers))
+          headers.release()
+        }
+        if (value.contains("body")) {
+          value.get("body") match {
+            case body: V8Value =>
+              builder.put("body", stringify(runtime, body))
+              body.release()
+            case body => builder.put("body", body.toString)
+          }
+        }
+        if (!value.contains("header") && !value.contains("body")) {
+          builder.put("headers", ImmutableMap.of("Content-Type", "application/json"))
+          builder.put("body", stringify(runtime, value))
+        }
+      }
+      value.release()
+      builder.build()
+    case _ => ImmutableMap.of("body", "")
+  }
+
+  private def stringify(runtime: V8, `object`: V8Value): String = {
+    val json = runtime.getObject("JSON")
+    val parameters = new V8Array(runtime).push(`object`)
+    try {
+      json.executeStringFunction("stringify", parameters)
+    } catch {
+      case _: Exception => `object`.toString
+    } finally {
+      parameters.release()
+      json.release()
+    }
   }
 }
 
